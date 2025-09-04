d82c86e18b96fa38e7c825c4e176eb177d035320
DESKTOP-EPDU8E5\Alin <rodica_munteanu@live.com>
2025-09-04 11:23:23 +0300

    first commit

--- src/com/itschool/homework/homework.java
+++ src/com/itschool/homework/homework.java
@@ -4,5 +4,6 @@ public class homework {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello again");
+        System.out.println("Modified");
     }
 }
